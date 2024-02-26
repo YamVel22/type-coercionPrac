87a763be6fd119914a9e1cc32366700d1267bfaf
Yamkelani Velaphi <106350341+YamVel22@users.noreply.github.com>
2024-02-26 10:18:32 +0200

    update

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 //Get users to enter their values
-
-let user1 = prompt("Enter your birth year");
-
-let user2= prompt("Enter your age");
+var user1 = prompt("Player 1: Enter a random number");
+var user2 = prompt("Player 2: Enter a random number");
 
 //compare the two values
 
+if (user1 > user2) {
+  console.log("You have won R" + user1 + "😁");
+} else {
+  alert("You are not a winner");
+}
